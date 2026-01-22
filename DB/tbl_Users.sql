8b5f2623e2a6d3394b8e2d4b398a10bbb64a62dd
Brian Huang <brianh+git@jswebinc.com>
2026-01-22 18:01:47 -0500

    User Table Creation

--- DB/tbl_Users.sql
+++ DB/tbl_Users.sql
@@ -1,11 +1,9 @@
-CREATE TABLE IF NOT EXISTS tbl_Users
-(
-    ID INT NOT NULL AUTO_INCREMENT,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    Login VARCHAR(50) NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (ID),
-    UNIQUE KEY uq_Login (Login)
-) ENGINE=InnoDB;
--- This table stores user information including first name, last name, login, and password.
+CREATE TABLE `COP4331`.`Users` 
+( 
+	`ID` INT NOT NULL AUTO_INCREMENT , 
+	`FirstName` VARCHAR(50) NOT NULL DEFAULT '' , 
+	`LastName` VARCHAR(50) NOT NULL DEFAULT '' , 
+	`Login` VARCHAR(50) NOT NULL DEFAULT '' , 
+	`Password` VARCHAR(50) NOT NULL DEFAULT '' , 
+	PRIMARY KEY (`ID`)
+) ENGINE = InnoDB;
