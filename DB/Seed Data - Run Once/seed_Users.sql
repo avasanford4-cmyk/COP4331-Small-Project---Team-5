b15c54e25668e420deaa05cee58a5b40003932c3
Brian Huang <brianh+git@jswebinc.com>
2026-02-21 19:30:54 -0500

    Removed old DB folder

--- DB/Seed Data - Run Once/seed_Users.sql
+++
@@ -1,9 +0,0 @@
--- Seed Data for Users Table
--- Run this BEFORE seed_Contacts.sql
-
-INSERT INTO `COP4331`.`Users` (`FirstName`, `LastName`, `Login`, `Password`) VALUES
-('John', 'Doe', 'johndoe', 'password123'),
-('Jane', 'Smith', 'janesmith', 'pass456'),
-('Mike', 'Johnson', 'mikej', 'secure789'),
-('Emily', 'Davis', 'emilyd', 'mypass321'),
-('Chris', 'Wilson', 'chrisw', 'wilson2024');
--- DB/view_All_Tables.sql
+++
@@ -1,7 +0,0 @@
--- View All Table Contents
-
--- View all Users
-SELECT * FROM `COP4331`.`Users`;
-
--- View all Contacts
-SELECT * FROM `COP4331`.`Contacts`;
